ec35f7a0a162f7649a0edc18c30ffc247e7615a7
Sahar Davidyan <Davidyan7@gmail.com>
2021-06-24 21:31:22 +0300

    change menu icon

--- js/cmps/app-header.js
+++ js/cmps/app-header.js
@@ -1,7 +1,8 @@
 export default {
     template: `
     <header class="app-header">
-        <img @click="setMenuOpen" calss="menu-icon" src="../../img/menu-icon.png" alt="">
+    <div @click="setMenuOpen" class="menu-icon">☰</div>
+        <!-- <img   src="../../img/menu-icon.png" alt=""> -->
         <nav v-if="isOpen" class="nav-bar-header">
             <router-link to="/" active-class="active-link" exact>Home</router-link> |
             <router-link to="/mail" >Mail</router-link> |
